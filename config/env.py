b2724a15fcb94bfaf122d653f47ba7ef6b6f6ec2
Petr Andreev <drakula2805@gmail.com>
2024-08-30 17:54:04 +0300

    fix env

--- config/env.py
+++ config/env.py
@@ -3,5 +3,5 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-STAGE = os.getenv('STAGE', default='qa')
+STAGE = os.getenv('STAGE', default='prod')
 HOST = 'https://dev-gs.qa-playground.com/api/v1' if STAGE == 'qa' else 'https://release-gs.qa-playground.com/api/v1'
